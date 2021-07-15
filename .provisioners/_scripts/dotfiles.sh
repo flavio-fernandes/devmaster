38e42b5c998737aff154897799603ef5ec537be9
Flavio Fernandes <flavio@flaviof.com>
2021-07-15 16:32:38 -0400

    Custom for flaviof

--- .provisioners/_scripts/dotfiles.sh
+++ .provisioners/_scripts/dotfiles.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-repo="jcaamano/dotfiles.git"
+repo="flavio-fernandes/dotfiles.git"
 repo_https="https://github.com/$repo"
 repo_git="git@github.com:$repo"
+branch="flaviof"
 
 [ -d "${HOME}/.dotfiles" ] && {
     cd ${HOME}/.dotfiles
-    git pull -f --depth 1 "$repo_https"
+    git pull -f --depth 1 "$repo_https" "$branch"
 } || {
-    git clone --depth 1 "$repo_https" ${HOME}/.dotfiles
+    git clone --no-single-branch --depth 1 "$repo_https" ${HOME}/.dotfiles
     cd ${HOME}/.dotfiles
     git remote add upstream "$repo_git"
+    [ -n "$branch" ] && git checkout "$branch"
 }
 
 stow zsh git code-server
